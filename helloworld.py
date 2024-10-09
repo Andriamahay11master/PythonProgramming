e9dcded8f4db89b60030835b6ecd99d3d5350b89
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-09 14:44:18 +0300

    add condition if else

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,11 @@
 nom = "Mahay"
 age = 32
+nombre_flottante = 3.14
 
 def hello():
-    print(f"Bonjour {nom} vous avez {age} ans")
+    if nom == "Mahay":
+        print(f"Hello, Mahay, tu as {age} ans")
+    else:
+        print("Hello, World")
 
 hello()
