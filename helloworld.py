a73dad322b8adc86b2d790c4ae605f8c821ac6c0
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-10 10:32:36 +0300

    learn liste

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,7 @@
 nom = "Mahay"
 age = 32
 nombre_flottante = 3.14
+plateformes_sociales = ["Facebook", "Instagram", "Twitter"]
 
 def hello():
     if nom == "Mahay":
@@ -9,3 +10,8 @@ def hello():
         print("Hello, World")
 
 hello()
+
+print("en sens normal",plateformes_sociales[0])
+print("en sens inverse",plateformes_sociales[-3])
+print("en sens normal",plateformes_sociales[0:2])
+print("chaine considéré comme un tableau de lettres",plateformes_sociales[0][0])
