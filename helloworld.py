4736e935ee5d804562297a0d9a7a1cceae88f87e
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-10 10:41:46 +0300

    learn append/remove/get size liste and sort liste

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 nom = "Mahay"
 age = 32
 nombre_flottante = 3.14
-plateformes_sociales = ["Facebook", "Instagram", "Twitter"]
+plateformes_sociales = ["Facebook", "Instagram", "Snapchat", "Twitter"]
 
 def hello():
     if nom == "Mahay":
@@ -15,3 +15,16 @@ print("en sens normal",plateformes_sociales[0])
 print("en sens inverse",plateformes_sociales[-3])
 print("en sens normal",plateformes_sociales[0:2])
 print("chaine considéré comme un tableau de lettres",plateformes_sociales[0][0])
+
+plateformes_sociales.append("TikTok")
+print(plateformes_sociales)
+
+plateformes_sociales.remove("Snapchat")
+print(plateformes_sociales)
+
+taille_liste = len(plateformes_sociales)
+print(taille_liste)
+
+plateformes_sociales.sort()
+plateformes_sociales.sort( reverse=True )
+print(plateformes_sociales)
