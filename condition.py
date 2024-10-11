3ee08cf7adbacf17909ee7bd4baf47b3386f4a13
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-11 14:37:08 +0300

    learn match case

--- condition.py
+++ condition.py
@@ -11,3 +11,19 @@ def check_password(algo_password):
 
 
 check_password(algo_password)
+
+fruit = "pomme"
+
+def check_fruit(fruit):
+    match fruit:
+        case "pomme":
+            print("Pomme")
+        case "cerise":
+            print("Cerise")
+        case "orange":
+            print("Orange")
+        case _:
+            print("Je ne le connais pas")
+
+
+check_fruit(fruit)
