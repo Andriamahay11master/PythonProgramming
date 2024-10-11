20af4dd5c47e01d5a9c48c2cbfae68e322de0041
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-11 14:55:47 +0300

    learn for in

---
+++ boucle.py
@@ -0,0 +1,8 @@
+liste_marque_voiture = ["audi", "bmw", "mercedes", "renault", "volkswagen"]
+
+def display_marque(liste):
+    for marque in liste:
+        print(marque)
+
+
+display_marque(liste_marque_voiture)
