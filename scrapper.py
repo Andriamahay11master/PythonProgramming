9170f119c2e6969853ffb32a3b4bb90b6b30c8f1
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-18 15:52:44 +0300

    use package Beautiful soup to extract parse html

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,13 @@
 import requests
 
+from bs4 import BeautifulSoup
+
 url = "https://www.gov.uk/search/news-and-communications"
 page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
 
-# Voir le code html source
-print(page.content)
+# Voir le code html source parse
+print(soup)
+
+#Voir le title de la page
+print(soup.title.text)
