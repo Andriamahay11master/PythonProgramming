9d3b7c640510128f0819ea0fb2261274149a0785
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-18 14:14:16 +0300

    init notion module & package

---
+++ main.py
@@ -0,0 +1,9 @@
+#import fonctions addition et multiplication from operations
+from operations import addition, multiplication
+
+#appel des fonctions
+somme = addition(10, 20)
+print(somme)
+
+multiplication = multiplication(5, 10)
+print(multiplication)
---
+++ operations.py
@@ -0,0 +1,7 @@
+#fonction pour additionner deux nombres
+def addition(a, b):
+    return a + b
+
+#fonction pour multiplier deux nombres
+def multiplication(a, b):
+    return a * b
