b4a5eebf97c0c14b7e4636b272a53f689ccb208e
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-18 16:02:00 +0300

    use find & find all

--- scrapper.py
+++ scrapper.py
@@ -7,7 +7,11 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
 # Voir le code html source parse
-print(soup)
+#print(soup)
 
 #Voir le title de la page
 print(soup.title.text)
+
+#Trouver et afficher les paragraphes ayant la class title
+print(soup.find("p", class_="gem-c-document-list__item-description"))
+print(soup.find_all("p", class_="gem-c-document-list__item-description"))
