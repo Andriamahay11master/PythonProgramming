0f6a15b3cae2e4059723271f31b51e267ab201be
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-21 11:54:35 +0300

    write and add text to one file

--- file.py
+++ file.py
@@ -1,3 +1,9 @@
+#write in file
+fichier = open("file.txt", "w")
+fichier.write("End of File")
+fichier.close()
+
+#add text in file
 fichier = open("file.txt", "a")
-fichier.write(" End of File")
+fichier.write("\nEnd of File")
 fichier.close()
